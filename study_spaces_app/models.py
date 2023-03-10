5b1070d04215796b5563f0717179a17de1565fec
boZhou <2571442z@student.gla.ac.uk>
2023-03-10 12:41:31 +0000

    parts of models.py

--- study_spaces_app/models.py
+++ study_spaces_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    NAME_MAX_LENGTH = 50
+    PASSWORD_MAX_LENGTH = 50
+    
+    userName = models.CharField(max_length=NAME_MAX_LENGTH,unique = True)
+    password = models.CharField(max_length=PASSWORD_MAX_LENGTH)
+    picture = models.ImageField(upload_to='profile_images',blank=True)
