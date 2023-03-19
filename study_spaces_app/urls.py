5d4c383256f35ec3781769df74b51d7d5c84dd6d
Xiaohan Mu <mxh126qq@126.com>
2023-03-19 04:05:17 +0000

    Add login/sign up page and base.html

--- study_spaces_app/urls.py
+++ study_spaces_app/urls.py
@@ -1,14 +1,12 @@
-from django.urls import include, path 
+from django.urls import path 
 from study_spaces_app import views
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
 
 
 app_name = 'study_spaces_app'
 
 urlpatterns = [
     path('', views.HomePage, name='HomePage'),
-    path('study_spaces_app/', include('study_spaces_app.urls')),
-     path('admin/', admin.site.urls),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('login',views.login_user,name = "Login"),
+    path('signup',views.register,name = "Signup"),
+    path('logout', views.logout_view, name='Logout'),
+]
--- study_spaces_app/views.py
+++ study_spaces_app/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse 
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required 
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from study_spaces_app.forms import UserForm, UserProfileForm
+from study_spaces_app.models import UserProfile, Post
 
 def HomePage(request):
     if request.user.is_authenticated:
@@ -11,3 +16,56 @@ def HomePage(request):
             'user_is_authenticated': False
         }
     return render(request, 'homepage.html', context=context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('Username')
+        password = request.POST.get('Password')
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect('study_spaces_app:HomePage')
+            else:
+                error_message = 'Your account is disabled.'
+        else:
+            error_message = 'Incorrect username or password. Please try again.'
+        context = {'error_message': error_message}
+    else:
+        context = {}
+    return render(request, 'login.html', context=context)
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+        
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            
+            user.set_password(user.password)
+            user.save()
+            
+            profile = profile_form.save(commit = False)
+            profile.user = user
+            
+            if 'user_profile' in request.FILES:
+                print("IMAGE HERE")
+                profile.user_profile = request.FILES['user_profile']
+                
+            profile.save()
+            
+            login_url = reverse('study_spaces_app:Login')
+            return redirect(login_url)
+        else:
+            print(user_form.errors,profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+    
+    return render(request,'signup.html',context = {'user_form':user_form,'profile_form':profile_form})
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('study_spaces_app:HomePage')
