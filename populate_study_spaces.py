18403e36afe04d2eefdee764f794b73e5ad2c58d
boZhou <2571442z@student.gla.ac.uk>
2023-03-19 09:28:40 +0000

    populate pictures and create static file and path

--- populate_study_spaces.py
+++ populate_study_spaces.py
@@ -1,17 +1,21 @@
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'study_spaces.settings')
 
+
 import django
 django.setup()
 
 from study_spaces_app.models import Comment, Category, Post, UserProfile
 from django.contrib.auth.models import User
+from django.contrib.staticfiles import finders
+from django.core.files import File
+
 
 
 def populate():
-    category1 = Category.objects.get_or_create(category_name='Cafe')[0]
-    category2 = Category.objects.get_or_create(category_name='Library')[0]
-    category3 = Category.objects.get_or_create(category_name='Other Places')[0]
+    category1 = Category.objects.get_or_create(category_name='Library')[0]
+    category2 = Category.objects.get_or_create(category_name='Other Place')[0]
+    category3 = Category.objects.get_or_create(category_name='cafe')[0]
 
     user_profile1 = createAccounts("user1","password111","Student","user1@email")
     user_profile2 = createAccounts("user2","password222","Student","user2@email")
@@ -25,6 +29,14 @@ def populate():
         user_profile = user_profile1
     )[0]
 
+
+
+    image_path = finders.find('images/James McCune Smith Learning Hub 066.jpg')
+    if image_path:
+        with open(image_path, 'rb') as f:
+            post1.picture.save(os.path.basename(image_path), File(f), save=True)
+
+
     post2 = Post.objects.get_or_create(
         pictureName='Fraser building',
         description='This is Fraser building.',
@@ -33,6 +45,11 @@ def populate():
         user_profile = user_profile2
     )[0]
 
+    image_path = finders.find('images/fraserBuilding.jpg')
+    if image_path:
+        with open(image_path, 'rb') as f:
+            post2.picture.save(os.path.basename(image_path), File(f), save=True)
+
     post3 = Post.objects.get_or_create(
         pictureName='University cafe',
         description='This is the University cafe.',
@@ -40,6 +57,11 @@ def populate():
         category=category3,
         user_profile=user_profile3
     )[0]
+
+    image_path = finders.find('images/university-cafe.jpg')
+    if image_path:
+        with open(image_path, 'rb') as f:
+            post3.picture.save(os.path.basename(image_path), File(f), save=True)
 
     comment1 = Comment.objects.get_or_create(
         comment='This is the comment for JMS.',
